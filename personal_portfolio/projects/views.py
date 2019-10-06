f9f61ff2e1ab5f8a956249dd6010a062a13f7eb9
Bao Pham <ryanvuepham@gmail.com>
2019-10-06 16:54:36 -0500

    creating projects urls.py

--- personal_portfolio/projects/views.py
+++ personal_portfolio/projects/views.py
@@ -4,9 +4,21 @@ from projects.models import Project
 # Create your views here.
 
 
-def project_index(requests):
-    projects = Project.objecs.all()
+def project_index(request):
+    # query - create, retrieve, update, or delete objects in database
+    projects = Project.objects.all()
+    # define dictionary context - only has 1 entry projects, assigns Queryset containing all projects
+    # context is used to send info to templates. every view func create needs to have a context dict.
     context = {
-        'projects': projects
+        "projects": projects
     }
-    return render(request, 'project_index.html', context)
+    return render(request, "project_index.html", context)
+
+
+def project_detail(request, pk):
+    # query - retrieve project with pk equal to that in func arg.
+    project = Project.objects.get(pk=pk)
+    context = {
+        'project': project
+    }
+    return render(request, 'project_detail.html', context)
